573dbecbc277033d4cd9d5b3e42b5e9b0b8e118a
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2023-12-09 10:06:57 +0100

    Update FullscreenNativeActivity.java

--- app/ruffle/src/main/java/rs/ruffle/FullscreenNativeActivity.java
+++ app/ruffle/src/main/java/rs/ruffle/FullscreenNativeActivity.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.swordfish.radialgamepad.*;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.view.ViewCompat;
@@ -34,10 +35,60 @@ import java.util.List;
 
 public class FullscreenNativeActivity extends GameActivity {
 
+    private lateinit var pad: RadialGamePad
+
+    private val padConfig = RadialGamePadConfig(
+        sockets = 6,
+        primaryDial = PrimaryDialConfig.Cross(
+            CrossConfig(
+                id = 0,
+                useDiagonals = false
+            )
+        ),
+        secondaryDials = listOf(
+            SecondaryDialConfig.SingleButton(
+                index = 1,
+                scale = 1f,
+                distance = 0f,
+                ButtonConfig(
+                    id = KeyEvent.KEYCODE_BUTTON_SELECT,
+                    iconId = R.drawable.ic_play
+                )
+            ),
+            SecondaryDialConfig.SingleButton(
+                index = 2,
+                scale = 1f,
+                distance = 0f,
+                ButtonConfig(
+                    id = KeyEvent.KEYCODE_BUTTON_L1,
+                    iconId = R.drawable.ic_stop
+                )
+            ),
+            SecondaryDialConfig.SingleButton(
+                index = 4,
+                scale = 1f,
+                distance = 0f,
+                ButtonConfig(
+                    id = KeyEvent.KEYCODE_BUTTON_MODE,
+                    iconId = R.drawable.ic_volume_down
+                )
+            ),
+            SecondaryDialConfig.SingleButton(
+                index = 5,
+                scale = 1f,
+                distance = 0f,
+                ButtonConfig(
+                    id = KeyEvent.KEYCODE_BUTTON_MODE,
+                    iconId = R.drawable.ic_volume_up
+                )
+            )
+        )
+    );
+
     static {
-        // load the native activity
         System.loadLibrary("ruffle_android");
     }
+
     protected static byte[] SWF_BYTES;
 
     protected byte[] getSwfBytes() {
@@ -49,6 +100,7 @@ public class FullscreenNativeActivity extends GameActivity {
     }
 
     int[] loc = new int[2];
+
     protected int[] getLocOnScreen() {
         mSurfaceView.getLocationOnScreen(loc);
         return loc;
@@ -63,20 +115,23 @@ public class FullscreenNativeActivity extends GameActivity {
     }
 
     private static native void keydown(byte key_code, char key_char);
+
     private static native void keyup(byte key_code, char key_char);
 
     private static native void resized();
 
     private static native String[] prepareContextMenu();
+
     private static native void runContextMenuCallback(int index);
+
     private static native void clearContextMenu();
 
     private static <T> List<T> gatherAllDescendantsOfType(View v, Class t) {
         List<T> result = new ArrayList<T>();
         if (t.isInstance(v))
-            result.add((T)v);
-        if (v instanceof  ViewGroup) {
-            ViewGroup vg = (ViewGroup)v;
+            result.add((T) v);
+        if (v instanceof ViewGroup) {
+            ViewGroup vg = (ViewGroup) v;
             for (int i = 0; i < vg.getChildCount(); ++i) {
                 result.addAll(gatherAllDescendantsOfType(vg.getChildAt(i), t));
             }
@@ -87,7 +142,8 @@ public class FullscreenNativeActivity extends GameActivity {
     @Override
     protected void onCreateSurfaceView() {
         LayoutInflater inflater = getLayoutInflater();
-        ConstraintLayout layout = (ConstraintLayout)inflater.inflate(R.layout.keyboard, null);
+        ConstraintLayout layout = (ConstraintLayout) inflater.inflate(R.layout.keyboard, null);
+
         this.contentViewId = ViewCompat.generateViewId();
         layout.setId(this.contentViewId);
         setContentView(layout);
@@ -96,20 +152,19 @@ public class FullscreenNativeActivity extends GameActivity {
 
         View placeholder = findViewById(R.id.placeholder);
 
-        ConstraintLayout.LayoutParams pars = (ConstraintLayout.LayoutParams)placeholder.getLayoutParams();
+        ConstraintLayout.LayoutParams pars = (ConstraintLayout.LayoutParams) placeholder.getLayoutParams();
 
-        ViewGroup parent = (ViewGroup)placeholder.getParent();
+        ViewGroup parent = (ViewGroup) placeholder.getParent();
         int index = parent.indexOfChild(placeholder);
         parent.removeView(placeholder);
         parent.addView(this.mSurfaceView, index);
         this.mSurfaceView.setLayoutParams(pars);
 
-
         List<Button> keys = gatherAllDescendantsOfType(layout.getViewById(R.id.keyboard), Button.class);
 
         for (Button b : keys) {
             b.setOnTouchListener((View view, MotionEvent motionEvent) -> {
-                String tag = (String)view.getTag();
+                String tag = (String) view.getTag();
                 if (tag != null) {
                     String[] spl = tag.split(" ", 2);
                     byte by = Byte.parseByte(spl[0]);
@@ -124,7 +179,7 @@ public class FullscreenNativeActivity extends GameActivity {
             });
         }
 
-        layout.findViewById(R.id.button_kb) .setOnClickListener((view) -> {
+        layout.findViewById(R.id.button_kb).setOnClickListener((view) -> {
             View keyboard = layout.getViewById(R.id.keyboard);
             if (keyboard.getVisibility() == View.VISIBLE) {
                 keyboard.setVisibility(View.GONE);
@@ -188,14 +243,10 @@ public class FullscreenNativeActivity extends GameActivity {
     }
 
     private void hideSystemUI() {
-        // This will put the game behind any cutouts and waterfalls on devices which have
-        // them, so the corresponding insets will be non-zero.
-        if (VERSION.SDK_INT >= VERSION_CODES.P) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             getWindow().getAttributes().layoutInDisplayCutoutMode
                     = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
         }
-        // From API 30 onwards, this is the recommended way to hide the system UI, rather than
-        // using View.setSystemUiVisibility.
         View decorView = getWindow().getDecorView();
         WindowInsetsControllerCompat controller = new WindowInsetsControllerCompat(getWindow(),
                 decorView);
@@ -203,22 +254,39 @@ public class FullscreenNativeActivity extends GameActivity {
         controller.hide(WindowInsetsCompat.Type.displayCutout());
         controller.setSystemBarsBehavior(
                 WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-
-
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // When true, the app will fit inside any system UI windows.
-        // When false, we render behind any system UI windows.
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         hideSystemUI();
-        // You can set IME fields here or in native code using GameActivity_setImeEditorInfoFields.
-        // We set the fields in native_engine.cpp.
-        // super.setImeEditorInfoFields(InputType.TYPE_CLASS_TEXT,
-        //     IME_ACTION_NONE, IME_FLAG_NO_FULLSCREEN );
         requestNoStatusBarFeature();
         super.onCreate(savedInstanceState);
+
+        pad = new RadialGamePad(padConfig, 8f, requireContext());
+        findViewById(FrameLayout.class).addView(pad);
+
+        lifecycleScope.launch {
+            pad.events()
+                .flowWithLifecycle(lifecycle, Lifecycle.State.RESUMED)
+                .collect {
+                    handleEvent(it);
+                }
+        }
+    }
+
+    private void handleEvent(Event event) {
+        switch (event) {
+            case BUTTON:
+                Log.d("Event", "Button event from control " + event.id);
+                break;
+            case GESTURE:
+                Log.d("Event", "Gesture event from control " + event.id);
+                break;
+            case DIRECTION:
+                Log.d("Event", "Direction event from control " + event.id);
+                break;
+        }
     }
 
     public boolean isGooglePlayGames() {
@@ -227,8 +295,8 @@ public class FullscreenNativeActivity extends GameActivity {
     }
 
     private void requestNoStatusBarFeature() {
-        //Hiding the status bar this way makes it see through when pulled down
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
-}
+            }
+    
