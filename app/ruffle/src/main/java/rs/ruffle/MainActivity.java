d6ee3f7711cc88cd467704357eee9c58a275caa8
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2024-01-01 22:15:08 +0100

    Update MainActivity.java

--- app/ruffle/src/main/java/rs/ruffle/MainActivity.java
+++ app/ruffle/src/main/java/rs/ruffle/MainActivity.java
@@ -12,7 +12,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class MainActivity extends AppCompatActivity {
+import com.erz.joysticklibrary.JoyStick;
+
+public class MainActivity extends AppCompatActivity implements JoyStick.JoyStickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,17 @@ public class MainActivity extends AppCompatActivity {
         requestNoStatusBarFeature();
         setContentView(R.layout.activity_main);
         hideActionBar();
+
+        JoyStick joy1 = (JoyStick) findViewById(R.id.joy1);
+        joy1.setListener(this);
+        joy1.setPadColor(Color.parseColor("#55ffffff"));
+        joy1.setButtonColor(Color.parseColor("#55ff0000"));
+
+        JoyStick joy2 = (JoyStick) findViewById(R.id.joy2);
+        joy2.setListener(this);
+        joy2.enableStayPut(true);
+        joy2.setPadBackground(R.drawable.pad);
+        joy2.setButtonDrawable(R.drawable.button);
 
         try {
             // Load tboi.swf directly from the assets folder
@@ -30,11 +43,33 @@ public class MainActivity extends AppCompatActivity {
 
             // Start FullscreenNativeActivity
             startActivity(new Intent(MainActivity.this, FullscreenNativeActivity.class));
-            finish(); // Optional: Finish the current activity after starting FullscreenNativeActivity
+            
         } catch (IOException e) {
             // Handle IOException
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void onMove(JoyStick joyStick, double angle, double power, int direction) {
+        switch (joyStick.getId()) {
+            case R.id.joy1:
+                gameView.move(angle, power);
+                break;
+            case R.id.joy2:
+                gameView.rotate(angle);
+                break;
+        }
+    }
+
+    @Override
+    public void onTap() {
+
+    }
+
+    @Override
+    public void onDoubleTap() {
+
     }
 
     private void requestNoStatusBarFeature() {
